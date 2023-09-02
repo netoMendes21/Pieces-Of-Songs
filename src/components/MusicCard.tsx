2aa1fa340dba43ccdc4358eda47b8fc38f50d635
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-09-02 15:42:57 -0300

    Requisito 5 finalizado

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { SongType } from '../types';
 
 type MusicCardProps = {
@@ -5,6 +6,9 @@ type MusicCardProps = {
 };
 
 function MusicCard({ NovoElemento }:MusicCardProps) {
+  const checado = '/src/images/checked_heart.png';
+  const naoChecado = '/src/images/empty_heart.png';
+  const [box, setBox] = useState(false);
   return (
     <div>
       <p>{NovoElemento.trackId }</p>
@@ -17,6 +21,22 @@ function MusicCard({ NovoElemento }:MusicCardProps) {
       <p>
         {NovoElemento.kind}
       </p>
+      <label
+        data-testid={ `checkbox-music-${NovoElemento.trackId}` }
+      >
+
+        <input
+          type="checkbox"
+          onChange={ () => setBox(!box) }
+          checked={ box }
+        />
+        <img
+          src={ box
+            ? checado
+            : naoChecado }
+          alt="favorite"
+        />
+      </label>
     </div>
   );
 }
